768842a7d808cb5dc2cfae71baf3e40092899c19
tema.tian <tema.tian@yamibuy.com>
2020-01-15 16:13:11 +0800

    Add druid

---
+++ 7.druid/src/main/java/com/tema/tian/druid/DruidApplication.java
@@ -0,0 +1,30 @@
+package com.tema.tian.druid;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@Slf4j
+public class DruidApplication implements CommandLineRunner {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DruidApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(dataSource.toString());
+	}
+}
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 ##### 4 spring-example
 ##### 5 单数据源
 ##### 6 多数据源
-##### 7 连接池HikariCP 
+##### 7 连接池Druid
