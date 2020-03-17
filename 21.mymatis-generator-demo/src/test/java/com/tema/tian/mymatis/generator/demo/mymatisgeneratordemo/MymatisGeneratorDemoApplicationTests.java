c8059465ead4054089fc63a42ab91688dd308103
tema.tian <tema.tian@yamibuy.com>
2020-03-17 10:12:07 +0800

    Add mybatis generator demo

---
+++ 21.mymatis-generator-demo/src/test/java/com/tema/tian/mymatis/generator/demo/mymatisgeneratordemo/MymatisGeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tema.tian.mymatis.generator.demo.mymatisgeneratordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymatisGeneratorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
